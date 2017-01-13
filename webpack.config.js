027f027747b24908d691cd4da68d390c9313e47f
AnnDon <majy89@126.com>
2017-01-13 17:19:22 +0800

    解决了js更改后需要手动刷新页面的问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 let path = require("path");
 let webpack = require("webpack");
 var node_modules = path.resolve(__dirname, 'node_modules');
-var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
-var pathToReactDOM = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js');
+//var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+//var pathToReactDOM = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js');
 
 module.exports = {
 	entry: [
-		'webpack/hot/only-dev-server',
 		'webpack/hot/dev-server',
 		'./app/index.js'
 	],
@@ -30,13 +29,12 @@ module.exports = {
 	resolve: {
 		extensions: ['', '.js', '.jsx'],
 		alias: {
-			'react': pathToReact,
-			'react-dom': pathToReactDOM
+			//'react': pathToReact,
+			//'react-dom': pathToReactDOM
 		}
 	},
 	plugins: [
 		new webpack.BannerPlugin('This file is created by Majy'),
-		new webpack.NoErrorsPlugin(),
-		new webpack.HotModuleReplacementPlugin()
+		new webpack.NoErrorsPlugin()
 	],
 }
